01ff5c8ea5cfd7743532939fcf66cf6241ba14a4
swethav08711 <shwe08711@gmail.com>
2021-12-15 14:57:44 +0530

    Tours

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import BirthDay from "./BirthdayReminder/BirthDay"
+import MainTours from "./Tours/MainTours"
+
+function App() {
+  return (
+    <>
+      {/* <BirthDay /> */}
+      <MainTours />
+    </>
+  )
+}
+
+export default App
